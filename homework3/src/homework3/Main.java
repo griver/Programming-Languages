8d63c60a7d89b532a4331f5871ade1aca568a161
Alexey Korovin <alexey.korovin@spbau.se>
2012-04-12 00:05:01 +0400

    Showcases

--- homework3/src/homework3/Main.java
+++ homework3/src/homework3/Main.java
@@ -8,10 +8,9 @@ import homework3.nfa.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,32 +22,70 @@ public class Main {
             NFABuilder builder = new NFABuilder(parameters);
             List<GrammarRule> rules = reader.read("input.txt");
 
-            Map<String, NFANode> automate = builder.build(rules);
+            NFA nfa = builder.build(rules);
 
-            NFA2DFAConverter converter =
-                    new NFA2DFAConverter(parameters, automate);
-            Map<Set<String>, DFANode> dfa = converter.convert();
-            DFAParameters dfaParameters = converter.getParameters();
+            NFA nfaU1 = builder.build(rules);
+            NFA nfaU2 = builder.build(rules);
+            nfaU2.removeEpsilon();
+            NFAUnifier unifier = new NFAUnifier(parameters);
+            NFA nfaUnion = unifier.unify(nfaU1, nfaU2);
+
+            NFA nfaCl = builder.build(rules);
+            NFAClosure.close(nfaCl);
+
+            // If we have NFA rather than DFA, we must
+            // first convert it to DFA before swapping
+            // states to get its complement.
+            NFA2DFAConverter converterComp = new NFA2DFAConverter(nfa);
+            DFA dfaComp = converterComp.getDFA();
+            DFAComplementer.complement(dfaComp);
+
+            NFA2DFAConverter converter = new NFA2DFAConverter(nfa);
+            DFA dfa = converter.getDFA();
 
             NFA2DOTPrinter printer = new NFA2DOTPrinter("outputNfa.dot");
-            printer.print(automate, parameters);
+            printer.print(nfa, nfa.getParameters());
+            printer.close();
+
+            printer = new NFA2DOTPrinter("outputNfaNoEps.dot");
+            nfa.removeEpsilon();
+            printer.print(nfa, nfa.getParameters());
+            printer.close();
+
+            printer = new NFA2DOTPrinter("outputNfaUnion.dot");
+            printer.print(nfaUnion, nfaUnion.getParameters());
+            printer.close();
+
+            printer = new NFA2DOTPrinter("outputNfaClosure.dot");
+            printer.print(nfaCl, nfaCl.getParameters());
             printer.close();
 
             DFA2DOTPrinter printerDfa = new DFA2DOTPrinter("outputDfa.dot");
-            printerDfa.print(dfa, dfaParameters);
+            printerDfa.print(dfa, dfa.getParameters());
+            printerDfa.close();
+
+            printerDfa = new DFA2DOTPrinter("outputDfaComplement.dot");
+            printerDfa.print(dfaComp, dfaComp.getParameters());
             printerDfa.close();
 
             System.out.println("Input the word: ");
             String word = in.nextLine();
 
-            NFAInterpreter interpreter = new NFAInterpreter(automate, parameters);
+            NFAInterpreter interpreter = new NFAInterpreter(nfa, nfa.getParameters());
             if(interpreter.checkWord(word)) {
                 System.out.println("Word: "+ word + " was accepted by NFA.");
             } else {
                 System.out.println("Word: "+ word + " was rejected by NFA.");
             }
 
-            DFAInterpreter dfaInterpreter = new DFAInterpreter(dfa, dfaParameters);
+            DFAInterpreter complementary = new DFAInterpreter(dfaComp, dfaComp.getParameters());
+            if(complementary.checkWord(word)) {
+                System.out.println("Word: "+ word + " was accepted by complemented DFA.");
+            } else {
+                System.out.println("Word: "+ word + " was rejected by complemented DFA.");
+            }
+
+            DFAInterpreter dfaInterpreter = new DFAInterpreter(dfa, dfa.getParameters());
             if(dfaInterpreter.checkWord(word)) {
                 System.out.println("Word: "+ word + " was accepted by DFA.");
             } else {
